fbd5475ad1535bb029bd9b03f79888daa6b65e35
Hao <lehaohust@gmail.com>
2018-05-06 22:50:58 +0700

    loading done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,22 @@
+
+/*            *  *   *    ****                    */
+/*            ****  ***   *  *                    */
+/*            *  * *   *  ****     Hao_dev_ekogi  */
+
+
+/*______________________________________________________________________________________
+__________________________________________________________________EKOGI LOADING_________
+________________________________________________________________________________________*/
+
+$(window).load(function() {
+    $('body').removeClass('preloading');
+    $('#preload').delay(1000).fadeOut('slow');
+});
+
+/*______________________________________________________________________________________
+________________________________________________EKOGI MENU TOGGLE AUTOMATICALLY_________
+________________________________________________________________________________________*/
+
 let ekogimenu = document.querySelector(".ekogi-menu");
 let ekogibutton = document.querySelector(".ekogi-menu__button");
 
@@ -14,15 +33,5 @@ let interactionPreview = setInterval(() => {
   toggleMenu();
 }, 2000)
 
-
-/*********************************/
-
-
-
-
-$(window).load(function() {
-    $('body').removeClass('preloading');
-    $('#preload').delay(1000).fadeOut('slow');
-});
 
 
