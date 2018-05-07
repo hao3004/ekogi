13d856c7fc21e39a116169c40dee4b63f58353e1
Hao <lehaohust@gmail.com>
2018-05-07 12:58:03 +0700

    slick done basic 07/05/18

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,10 +38,38 @@ _______________________________________EKOGI SLICK SLIDER - lib-type: automatic_
 ________________________________________________________________________________________*/
 $(document).ready(function(){
     $('.esogi-slider-autoplay').slick({
-      slidesToShow: 3,
+      slidesToShow: 5,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 1500,
-      arrows: false
+      arrows: false/*,
+      responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]*/
     });
 });
