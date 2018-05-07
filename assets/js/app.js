55e96dc17888e070c9d26ca1891acb568b55d9a2
Hao <lehaohust@gmail.com>
2018-05-07 12:27:08 +0700

    slick demo

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,5 +33,15 @@ let interactionPreview = setInterval(() => {
   toggleMenu();
 }, 2000)
 
-
-
+/*______________________________________________________________________________________
+_______________________________________EKOGI SLICK SLIDER - lib-type: automatic_________
+________________________________________________________________________________________*/
+$(document).ready(function(){
+    $('.esogi-slider-autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false
+    });
+});
